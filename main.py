1c23861c45db674ab0696f40af336861ecdec96d
FelipeCiv04 <felipe.civ04@gmail.com>
2024-04-30 17:51:24 -0300

    Tarefa do dia 1
    Usando print e variáveis

---
+++ main.py
@@ -0,0 +1,5 @@
+print("Bem vindo ao gerador de nomes para sua banda")
+city_name = input(str("Qual o nome da cidade que você mora? \n"))
+pet_name = input(str("Qual o nome do seu pet? \n"))
+
+print("o nome da sua banda poderia ser",pet_name, "de",city_name)
